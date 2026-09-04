e8aac08b6c36e15ad05024fc418d437ed848a195
Richard-777-G <richardmanovishaal@gmail.com>
2026-09-04 15:31:45 +0530

    add domain phase model

---
+++ src/lib/abroadshield/phase.ts
@@ -0,0 +1,3 @@
+export type PhaseId = "pre-departure" | "arrival" | "studying" | "job-success";
+
+export const VALID_PHASES: readonly PhaseId[] = ["pre-departure", "arrival", "studying", "job-success"];
